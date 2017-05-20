1733ac9dc572040154378647c589ca49488355cb
Martin Bojnanský <martin.bojnansky@outlook.com>
2017-05-20 21:56:46 +0000

    Deleted /NewsPicker.Api

--- NewsPicker.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewsPicker.Api.WebApiApplication" Language="C#" %>
--- NewsPicker.Api/Resources/Scripts/swagger_apikey.js
+++
@@ -1,7 +0,0 @@
-﻿$(function () {
-    $('#input_apiKey').off();
-    $('#input_apiKey').on('change',
-        function () {
-            window.authorizations.add('key', new ApiKeyAuthorization('Authorization', this.value, 'header'));
-        });
-})();
