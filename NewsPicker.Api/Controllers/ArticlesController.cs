2dd41710c21057880a0eec676998085aebb53252
Martin Bojnanský <martin.bojnansky@outlook.com>
2017-06-11 12:40:17 +0200

    API update.

--- NewsPicker.Api/Controllers/ArticlesController.cs
+++ NewsPicker.Api/Controllers/ArticlesController.cs
@@ -10,7 +10,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using NewsPicker.Shared.Enums;
+using NewsPicker.Shared.Models;
 using NewsPicker.Api.Authorization;
 using NewsPicker.Shared.DTO.Article;
 using NewsPicker.Database.Models;
@@ -24,39 +24,39 @@ namespace NewsPicker.Api.Controllers
 
         [HttpGet]
         [ResponseType(typeof(List<ArticleDTO>))]
-        public List<ArticleDTO> GetTopArticlesByCategoryId(int categoryId, TimePeriod timePeriod = TimePeriod.DAY)
+        public List<ArticleDTO> GetTopArticlesByCategoryId(int categoryId, TimePeriodValue timePeriodValue = TimePeriodValue.DAY)
         {
             IQueryable<Article> articles;
 
             // Filter by category
             articles = db.Articles.Where(a => a.Source.Categories.FirstOrDefault(c => c.Id == categoryId) != null);
-            articles = GetTopArticles(articles, timePeriod);
+            articles = GetTopArticles(articles, timePeriodValue);
 
             return articles.ProjectTo<ArticleDTO>().ToList();
         }
 
         [HttpGet]
         [ResponseType(typeof(List<ArticleDTO>))]
-        public List<ArticleDTO> GetTopArticlesByCountryId(int countryId, TimePeriod timePeriod = TimePeriod.DAY)
+        public List<ArticleDTO> GetTopArticlesByCountryId(int countryId, TimePeriodValue timePeriodValue = TimePeriodValue.DAY)
         {
             IQueryable<Article> articles;
 
             // Filter by country
             articles = db.Articles.Where(a => a.Source.Categories.FirstOrDefault(c => c.CountryId == countryId) != null);
-            articles = GetTopArticles(articles, timePeriod);
+            articles = GetTopArticles(articles, timePeriodValue);
 
             return articles.ProjectTo<ArticleDTO>().ToList();
         }
 
         [NonAction]
-        private IQueryable<Article> GetTopArticles(IQueryable<Article> articles, TimePeriod timePeriod)
+        private IQueryable<Article> GetTopArticles(IQueryable<Article> articles, TimePeriodValue timePeriodValue)
         {
-            DateTime? startDate = DateTime.UtcNow.AddHours(-(int)timePeriod);
+            DateTime? startDate = DateTime.UtcNow.AddHours(-(int)timePeriodValue);
 
             // Filter by date period
             articles = articles.Where(a => startDate <= a.CreatedDate);
             // Order by share count
-            articles = articles.OrderByDescending(a => a.ShareCount);
+            articles = articles.OrderByDescending(a => a.EngagementCount);
             // Take top 10
             articles = articles.Take(10);
 
