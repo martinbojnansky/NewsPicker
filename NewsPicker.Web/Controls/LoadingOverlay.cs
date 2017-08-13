b983da6b84e2dde7925f88a06ccd39dfdf94ab3a
Martin Bojnanský <martin.bojnansky@outlook.com>
2017-08-13 22:25:41 +0200

    Loading overlay.

---
+++ NewsPicker.Web/Controls/LoadingOverlay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.Controls;
+
+namespace NewsPicker.Web.Controls
+{
+    public class LoadingOverlay : DotvvmMarkupControl
+    {
+    }
+}
